af4dd0da3e3ffe1706ab0936a850f11b8d4a2e25
Anton Yastrebkov <anton.yastrebkoff@gmail.com>
2020-05-29 20:48:31 +0300

    Object as mapDispatchToProps, lesson 128

--- src/actions.js
+++ src/actions.js
@@ -2,4 +2,10 @@ export const inc = () => ({ type: 'INC' });
 
 export const dec = () => ({ type: 'DEC' });
 
-export const rnd = (payload) => ({ payload, type: 'RND' });
+export const rnd = () => {
+    const payload = Math.floor(Math.random() * 10);
+    return {
+        type: 'RND',
+        payload
+    };
+};
