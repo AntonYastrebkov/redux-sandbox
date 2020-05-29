0be74b03d5909113678a02e5c89e2ef48cb8ed79
Anton Yastrebkov <anton.yastrebkoff@gmail.com>
2020-05-29 16:57:33 +0300

    Simple clicker with redux, lesson 123

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-const reducer = (state = 0, action) => {
+import { createStore } from 'redux';
 
-    switch(action.type) {
-        case 'INC':
-            return state + 1;
-        default:
-            return state;
-    }
+import reducer from './reducer';
+import { inc, dec, rnd } from './actions';
+
+const store = createStore(reducer);
+
+document.getElementById('inc')
+    .addEventListener('click', () => {
+        store.dispatch(inc());
+    });
+
+document.getElementById('dec')
+    .addEventListener('click', () => {
+        store.dispatch(dec());
+    });
+
+document.getElementById('rnd')
+    .addEventListener('click', () => {
+        const payload = Math.floor(Math.random() * 10);
+        store.dispatch(rnd(payload));
+    });
+
+const update = () => {
+    document.getElementById('counter')
+        .innerHTML = store.getState();
 };
 
-let state = reducer(undefined, {});
-
-state = reducer(state, { type: 'INC' });
-console.log(state);
+store.subscribe(update);
